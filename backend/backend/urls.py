a11d0bd68f71fbad4b789082540361c1db6071e5
MrZhengXin <510546841@qq.com>
2020-08-05 12:55:00 +0800

    login api update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     # path('api/', include(router.urls)),
     # path('', include('todo.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('accounts/', include('rest_registration.api.urls')),
+    path('account/', include('rest_registration.api.urls')),
     url(r'^upload/$', views.FileUploadView.as_view(), name='file-upload'),
  
     url(r'^download/tbATUC2I', views.DownloadTbatuc2IView.as_view(), name='tbATUC2I download'),
